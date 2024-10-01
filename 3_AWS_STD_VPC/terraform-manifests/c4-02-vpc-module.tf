66660d96de037e418a69105e9d5ec3e3fd523005
Mukesh441 <sethi.mukeshjain@gmail.com>
2024-10-01 16:51:54 +0530

    few changes added

--- 3_AWS_STD_VPC/terraform-manifests/c4-02-vpc-module.tf
+++ 3_AWS_STD_VPC/terraform-manifests/c4-02-vpc-module.tf
@@ -4,20 +4,20 @@ module "vpc" {
 
   # VPC Basic Details
   #vpc_name = "vpc-dev"                         # Use 'vpc_name' instead of 'name'
-  cidr     = "10.0.0.0/16"                     # 'cidr' is valid
+  cidr     = var.vpc_cidr_block //"10.0.0.0/16"                     # 'cidr' is valid
 
-  azs                 = ["us-east-1a", "us-east-1b"]
-  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs                 = var.vpc_availability_zones //["us-east-1a", "us-east-1b"]
+  private_subnets     = var.vpc_private_subnets//["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets      = var.vpc_public_subnets //["10.0.101.0/24", "10.0.102.0/24"]
 
   # Database Subnets
-  database_subnets    = ["10.0.151.0/24", "10.0.152.0/24"]
+  database_subnets    = var.vpc_database_subnets //["10.0.151.0/24", "10.0.152.0/24"]
   create_database_subnet_group = true
   create_database_subnet_route_table= true
   create_database_nat_gateway_route = false      # Adjust argument for creating NAT gateway route
     # Assign a value to 'database_subnet_group_name' and 'name'
   database_subnet_group_name = "my-db-subnet-group"
-  name                       = "vpc-dev"
+  name                       = "${local.name}-${var.vpc_name}"//"vpc-dev"
 
   # NAT Gateways - Outbound Communication
   enable_nat_gateway = true                     # This is correct
@@ -41,12 +41,14 @@ module "vpc" {
   }
 
   # Tags for VPC and Resources
-  tags = {
+  tags = local.common_tags
+   /*{
     Owner       = "Mukesh"
     Environment = "dev"
-  }
+  } */
 
-  vpc_tags = {
+  vpc_tags = local.common_tags
+  /*{
     Name = "vpc-dev"
-  }
+  }*/
 }
